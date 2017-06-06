010d9b0589108cb07fa0d141c0c0fa1b6f66f499
Affandhy <achmad_affandhy_24rpl@student.smktelkom-mlg.sch.id>
2017-06-06 13:15:25 +0700

    navigasi1 commit

--- app/src/main/java/xir3n02/remidiuts/telkomschid/navigasiku/MainActivity.java
+++ app/src/main/java/xir3n02/remidiuts/telkomschid/navigasiku/MainActivity.java
@@ -1,7 +1,9 @@
 package xir3n02.remidiuts.telkomschid.navigasiku;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.frag1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        });
     }
 }
